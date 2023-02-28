30bf628abe18396ac69fef2855258c71b075f8b7
ddddddeon <chris.deon@unity3d.com>
2023-02-27 20:57:30 -0500

    formatting tweaks

--- src/request.rs
+++ src/request.rs
@@ -13,15 +13,16 @@ use serde_json::{Value, from_str};
 
 pub fn make_request(url: String, prompt: String) -> Result<String, Box<dyn std::error::Error>> {
     let api_key = &env::var("OPENAI_API_KEY")?;
-    let max_tokens = 4097 - prompt.len() as u32;
-    let body = format!("{{ \"model\": \"text-davinci-003\", \"prompt\": \"{prompt}\", \"temperature\": 0.2, \"max_tokens\": {max_tokens} }}");
-    let mut headers = HeaderMap::new();
+    let max_tokens = 4097 - prompt.len() as u32; //TODO ??
+
     let mut auth = String::from("Bearer ");
     auth.push_str(api_key);
 
-    let header = HeaderValue::from_str(auth.as_str())?;
-    headers.insert("Authorization", header);
+    let mut headers = HeaderMap::new();
+    headers.insert("Authorization", HeaderValue::from_str(auth.as_str())?);
     headers.insert("Content-Type", HeaderValue::from_str("application/json")?);
+
+    let body = format!("{{ \"model\": \"text-davinci-003\", \"prompt\": \"{prompt}\", \"temperature\": 0.2, \"max_tokens\": {max_tokens} }}");
 
     let client = reqwest::blocking::Client::new();
     let mut res = client.post(url)
@@ -29,10 +30,10 @@ pub fn make_request(url: String, prompt: String) -> Result<String, Box<dyn std::
         .headers(headers)
         .send()?;
 
-    let mut body = String::new();
-    res.read_to_string(&mut body)?;
+    let mut response_body = String::new();
+    res.read_to_string(&mut response_body)?;
 
-    let json_object: Value = from_str(&body)?;
+    let json_object: Value = from_str(&response_body)?;
     let answer = json_object.get("choices")
         .and_then(|value| value.get(0))
         .and_then(|value| value.get("text"))
