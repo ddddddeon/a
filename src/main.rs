f2693e0736f0c7c630871c2f70b04eb522497729
ddddddeon <chris.deon@unity3d.com>
2023-02-28 20:02:53 -0500

    check for existence of language syntax

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,51 @@
 use bat::PrettyPrinter;
+use bat::Syntax;
 use copypasta_ext::prelude::*;
 use copypasta_ext::x11_fork::ClipboardContext;
 
 pub mod request;
 
+fn lang_exists(lang: &String, langs: &Vec<Syntax>) -> bool {
+    for l in langs {
+        if &l.name == lang {
+            return true;
+        }
+    }
+    false
+}
+
 fn main() {
-    let mut args: Vec<_> = std::env::args().collect();
-    args.remove(0);
+     let mut args: Vec<_> = std::env::args().collect();
+     args.remove(0);
 
-    if args.len() == 0 {
-        println!("no prompt provided");
-        std::process::exit(1);
-    }
+     if args.len() == 0 {
+         println!("no prompt provided");
+         std::process::exit(1);
+     }
 
-    let lang = args[0].clone();
-    let prompt = args.join(" ");
-    let url = String::from("https://api.openai.com/v1/completions");
+     let mut lang = args[0].clone();
+     let prompt = args.join(" ");
+     let url = String::from("https://api.openai.com/v1/completions");
 
-    let response = request::make_request(url, prompt).expect("Could not make request to API");
+     let response = request::make_request(url, prompt).expect("Could not make request to API");
 
-    let mut response = String::from(response.strip_prefix("\n\n").unwrap());
-    response.push_str("\n");
+     let mut response = String::from(response.strip_prefix("\n\n").unwrap());
+     response.push_str("\n");
 
-    #[cfg(feature = "clipboard")]
-    {
-        let mut ctx = ClipboardContext::new().unwrap();
-        ctx.set_contents(response.clone()).unwrap();
-    }
+     #[cfg(feature = "clipboard")]
+     {
+         let mut ctx = ClipboardContext::new().unwrap();
+         ctx.set_contents(response.clone()).unwrap();
+     }
 
-    PrettyPrinter::new()
-        .input_from_bytes(response.as_bytes())
+     let mut pp = PrettyPrinter::new();
+
+     let langs: Vec<_> = pp.syntaxes().collect();
+     if !lang_exists(&lang, &langs) {
+         lang = String::from("txt");
+     }
+
+    pp.input_from_bytes(response.as_bytes())
         .language(&lang)
         .print()
         .unwrap();
