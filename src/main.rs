fa17b64144dae392b38e3d074f136c2c960f077c
ddddddeon <chris.deon@unity3d.com>
2023-02-28 22:38:15 -0500

    formatting

--- src/main.rs
+++ src/main.rs
@@ -15,35 +15,35 @@ fn lang_exists(lang: &String, langs: &Vec<Syntax>) -> bool {
 }
 
 fn main() {
-     let mut args: Vec<_> = std::env::args().collect();
-     args.remove(0);
+    let mut args: Vec<_> = std::env::args().collect();
+    args.remove(0);
 
-     if args.len() == 0 {
-         println!("no prompt provided");
-         std::process::exit(1);
-     }
+    if args.len() == 0 {
+        println!("no prompt provided");
+        std::process::exit(1);
+    }
 
-     let mut lang = args[0].clone();
-     let prompt = args.join(" ");
-     let url = String::from("https://api.openai.com/v1/completions");
+    let mut lang = args[0].clone();
+    let prompt = args.join(" ");
+    let url = String::from("https://api.openai.com/v1/completions");
 
-     let response = request::make_request(url, prompt).expect("Could not make request to API");
+    let response = request::make_request(url, prompt).expect("Could not make request to API");
 
-     let mut response = String::from(response.strip_prefix("\n\n").unwrap());
-     response.push_str("\n");
+    let mut response = String::from(response.strip_prefix("\n\n").unwrap());
+    response.push_str("\n");
 
-     #[cfg(feature = "clipboard")]
-     {
-         let mut ctx = ClipboardContext::new().unwrap();
-         ctx.set_contents(response.clone()).unwrap();
-     }
+    #[cfg(feature = "clipboard")]
+    {
+        let mut ctx = ClipboardContext::new().unwrap();
+        ctx.set_contents(response.clone()).unwrap();
+    }
 
-     let mut pp = PrettyPrinter::new();
+    let mut pp = PrettyPrinter::new();
 
-     let langs: Vec<_> = pp.syntaxes().collect();
-     if !lang_exists(&lang, &langs) {
-         lang = String::from("txt");
-     }
+    let langs: Vec<_> = pp.syntaxes().collect();
+    if !lang_exists(&lang, &langs) {
+        lang = String::from("txt");
+    }
 
     pp.input_from_bytes(response.as_bytes())
         .language(&lang)
